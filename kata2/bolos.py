808f94772651b238d8ebe009806c2c13da5b6642
José Manuel Martínez <jm.martinez23@usp.ceu.com>
2020-12-25 12:18:17 +0100

    Test ronda con un strike y punto OK

--- kata2/bolos.py
+++ kata2/bolos.py
@@ -1,3 +1,7 @@
 class Partida():
 	def prueba(self, ronda):
-		return sum(ronda)
+		puntos_extra = 0
+		for i in range(0, len(ronda)):
+			if ronda[i] == 10:
+				puntos_extra += ronda[i+1]
+		return sum(ronda) + puntos_extra
