bc6d2882ce3d1b8535a9d93d518c5bf48c6c2bd6
José Manuel Martínez <jm.martinez23@usp.ceu.com>
2020-12-28 15:56:39 +0100

    Test ronda con strike y puntos

--- kata3/test-bolos.py
+++ kata3/test-bolos.py
@@ -13,3 +13,9 @@ class TestBolos(unittest.TestCase):
 		ronda = [1,3,2,4,0,4,0,4,0,0,0,2,0,2,0,0,6,0,0,8,0]
 		puntuacion = partida.puntuacion(ronda)
 		self.assertEqual(puntuacion, 36)
+
+	def test_ronda_con_strike_y_puntos(self):
+		partida = Partida()
+		ronda = [10,0,3,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		puntuacion = partida.puntuacion(ronda)
+		self.assertEqual(puntuacion, 26)
